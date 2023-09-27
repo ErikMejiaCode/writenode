1e7cc40450fe4d640fe5940cbae3e7b7734fca97
Erik Mejia <erikdojo352@gmail.com>
2023-09-27 12:53:47 -0400

    firebase configuration

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,22 @@
+import { useEffect, useState } from "react";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../firebase/config";
 import { PostCard } from "../components";
 
 export const HomePage = () => {
-  const posts = [
-    {
-      id: 1,
-      title: "lorem ipsum dolor sit amet",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus eum optio velit molestiae eveniet pariatur accusamus architecto quidem. Cum ipsam unde pariatur dicta hic? Unde saepe iure quas nihil quisquam numquam, sequi facilis nostrum labore consequatur obcaecati fuga similique, provident, culpa dolorem. Maxime, alias perspiciatis.",
-      author: "Erik",
-    },
-    {
-      id: 2,
-      title: "lorem ipsum dolor asodiitit",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus eum optio velit molestiae eveniet pariatur accusamus architecto quidem. Cum ipsam unde pariatur dicta hic? Unde saepe iure quas nihil quisquam numquam, sequi facilis nostrum labore consequatur obcaecati fuga similique, provident, culpa dolorem. Maxime, alias perspiciatis.",
-      author: "Erik",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+  const postReference = collection(db, "posts");
+
+  useEffect(() => {
+    async function getPosts() {
+      const data = await getDocs(postReference);
+      setPosts(
+        data.docs.map((document) => ({ ...document.data(), id: document.id }))
+      );
+    }
+    console.log("------");
+    getPosts();
+  }, []);
 
   return (
     <section>
